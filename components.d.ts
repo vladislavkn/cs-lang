45e97d618f1b5c738d8af34e64834f8888a7bd52
Vladislav Knyshov <inova136@gmail.com>
2024-03-09 20:14:45 +0100

    feat: add builded files

--- components.d.ts
+++ components.d.ts
@@ -8,16 +8,13 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     AButton: typeof import('ant-design-vue/es')['Button']
-    ADivider: typeof import('ant-design-vue/es')['Divider']
     AFlex: typeof import('ant-design-vue/es')['Flex']
     AInput: typeof import('ant-design-vue/es')['Input']
     ATable: typeof import('ant-design-vue/es')['Table']
     ATypographyText: typeof import('ant-design-vue/es')['TypographyText']
     ATypographyTitle: typeof import('ant-design-vue/es')['TypographyTitle']
-    CodeInfo: typeof import('./src/components/CodeInfo.vue')['default']
     CodeTextarea: typeof import('./src/components/CodeTextarea.vue')['default']
     Heap: typeof import('./src/components/Heap.vue')['default']
-    HeapSnapshot: typeof import('./src/components/HeapSnapshot.vue')['default']
     StatItem: typeof import('./src/components/StatItem.vue')['default']
     StatList: typeof import('./src/components/StatList.vue')['default']
     Toolbar: typeof import('./src/components/Toolbar.vue')['default']
